584c161e50dbdf9ba4c0cfed7e69b92b069bb41b
Damiano Fusco <Damiano.Fusco@tribridge.com>
2016-09-29 13:46:11 -0400

    Initial commit

---
+++ grunt/tasks/eslint.js
@@ -0,0 +1,8 @@
+module.exports = {
+    options: {
+        configFile: './.eslintrc',
+        reset: true,
+        //rulePaths: ['conf/rules']
+    },
+    target: ['./lib/**/*.js']
+};
---
+++ grunt/tasks/sass.js
@@ -0,0 +1,10 @@
+module.exports = {
+    options: {
+        sourceMap: false
+    },
+    dist: {
+        files: {
+            'lib/css/main.css': 'sass/main.scss'
+        }
+    }
+};
